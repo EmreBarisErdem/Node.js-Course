af083fbe1e171e1d9065a093ffef1f2ff016a245
Emre Baris Erdem <erdem.emre.baris@gmail.com>
2025-01-30 12:17:26 +0300

    Reset Password işlemi gerçekleştirildi.

--- controllers/account.js
+++ controllers/account.js
@@ -178,6 +178,67 @@ exports.getLogout = (req, res, next) => {
         console.log(err);
         res.redirect('/');
     })
+}
+
+exports.getNewPassword = (req, res, next) => {
+
+    var errorMessage = req.session.errorMessage;
+    delete req.session.errorMessage;
+
+    const token = req.params.token;
+
+    User.findOne(
+        {
+            resetToken: token, 
+            resetTokenExpiration: {$gt: Date.now()}
+        }
+    ).then(user => {
+ 
+        res.render('account/new-password',{
+            path:'/new-password',
+            title: 'New Password',
+            errorMessage: errorMessage,
+            userId: user._id.toString(),
+            passwordToken: token
+        });
+
+    }).catch(err => {
+        console.log(err);
+    });
+        
+    
+}
+
+exports.postNewPassword = (req, res, next) => {
+    const newPassword = req.body.password;
+    const userId = req.body.userId;
+    const token = req.body.passwordToken;
+    let _user;
+
+    User.findOne(
+        {
+            resetToken: token, 
+            resetTokenExpiration: {$gt: Date.now()},
+            _id: userId
+        }
+    ).then(user => {
+
+        _user = user;
+        return bcrypt.hash(newPassword, 10);
+
+    }).then((hashedPassword)=>{
+
+        _user.password = hashedPassword;
+        _user.resetToken = undefined;
+        _user.resetTokenExpiration = undefined;
+        return _user.save();
+
+    }).then(()=>{
+        res.redirect('/login');
+    })
+    .catch(err => {console.log(err);});
+
+
 
     
 }
